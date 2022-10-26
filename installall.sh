00c20ee52642e51e08d03d70a50438dc2f928129
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-10-26 12:56:34 +0300

    Update installall.sh

--- installall.sh
+++ installall.sh
@@ -16,28 +16,28 @@ apt install curl -y
 echo "Downloading And Executing Offical Procurus Script From Coolstar"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/paradigmer/Uncursus/new/procursus-deploy-u0.sh)"
 echo "Downloading And Installing Offical Procurus Deb"
-rm -rf /User/Documents/Uncursus
-mkdir /User/Documents/Uncursus
+rm -rf /tmp/Uncursus
+mkdir /tmp/Uncursus
 apt update
 apt install wget -y --allow-unauthenticated
-wget https://raw.githubusercontent.com/paradigmer/Uncursus/new/DebProcurusSystem.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
-unzip /User/Documents/Uncursus/DebProcurusSystem.zip?raw=true -d /User/Documents/Uncursus/
-dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
+wget https://raw.githubusercontent.com/paradigmer/Uncursus/new/DebProcurusSystem.zip?raw=true --directory-prefix=/tmp/Uncursus/
+unzip /tmp/DebProcurusSystem.zip?raw=true -d /tmp/Uncursus/
+dpkg -i /tm/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
-mkdir /User/Documents/Uncursus/u0
-wget https://raw.githubusercontent.com/paradigmer/Uncursus/new/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
-unzip /User/Documents/Uncursus/DebPatch.zip?raw\=true -d /User/Documents/Uncursus/DebPatch
+mkdir /tmp/u0
+wget https://raw.githubusercontent.com/paradigmer/Uncursus/new/DebPatch.zip?raw=true --directory-prefix=/tmp/Uncursus/
+unzip /tmp/Uncursus/DebPatch.zip?raw\=true -d /tmp/Uncursus/DebPatch
 rm -rf /usr/bin/cynject
-wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
-wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
+wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/tmp/u0
+wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/tmp/Uncursus/u0
 echo "Done. Installing necessary debs for patch."
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/us.diatr.sileorespring_1.1_iphoneos-arm.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/coreutils-bin.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/libssl.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/lzma.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/ncurses5-libs.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/xz.deb
-dpkg -i --force-all /User/Documents/Uncursus/u0/*.deb
+dpkg -i --force-all /tmp/Uncursus/DebPatch/us.diatr.sileorespring_1.1_iphoneos-arm.deb
+dpkg -i --force-all /tmp/Uncursus/DebPatch/coreutils-bin.deb
+dpkg -i --force-all /tmp/Uncursus/DebPatch/libssl.deb
+dpkg -i --force-all /tmp/Uncursus/DebPatch/lzma.deb
+dpkg -i --force-all /tmp/Uncursus/DebPatch/ncurses5-libs.deb
+dpkg -i --force-all /tmp/Uncursus/DebPatch/xz.deb
+dpkg -i --force-all /tmp/Uncursus/u0/*.deb
 echo "Done. Running Firmware Configuration (./firmware.sh)"
 /usr/libexec/firmware
 echo "BootStrap Installions Done. The Installer Clean The Installions"
