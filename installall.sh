02bdd69d94546005020ce2715e4c723db8f97872
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-10-26 22:02:30 +0300

    Update installall.sh

--- installall.sh
+++ installall.sh
@@ -21,12 +21,12 @@ mkdir /tmp/Uncursus
 apt update
 apt install wget -y --allow-unauthenticated
 wget https://raw.githubusercontent.com/paradigmer/Uncursus/new/DebProcurusSystem.zip?raw=true --directory-prefix=/tmp/Uncursus/
-unzip /tmp/DebProcurusSystem.zip?raw=true -d /tmp/Uncursus/
+unzip /tmp/DebProcurusSystem.zip -d /tmp/Uncursus/
 dpkg -i /tm/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
 mkdir /tmp/u0
 wget https://raw.githubusercontent.com/paradigmer/Uncursus/new/iphoneos-arm64.zip?raw=true --directory-prefix=/tmp/Uncursus/
-unzip /tmp/Uncursus/DebPatch.zip?raw\=true -d /tmp/Uncursus/DebPatch
+unzip /tmp/Uncursus/DebPatch.zip -d /tmp/Uncursus/DebPatch
 rm -rf /usr/bin/cynject
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/tmp/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/tmp/Uncursus/u0
