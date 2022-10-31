5768b55f2b72d1cb4eb5228d1829bab35d395581
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-10-31 21:13:30 +0300

    Update installall.sh

--- installall.sh
+++ installall.sh
@@ -27,6 +27,7 @@ unzip /User/Documents/Uncursus/iphoneos-arm64.zip?raw\=true -d /User/Documents/U
 rm -rf /usr/bin/cynject
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_2.3.1_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
+Dpkg -i /User/Documents/Uncursus/u0/*.deb
 echo "Done. Installing necessary debs for patch."
 
 echo "Done. Running Firmware Configuration (./firmware.sh)"
