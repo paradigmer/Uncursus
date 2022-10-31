ec5a6e6da774405c05c34bea07ca55771b0c6478
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-10-31 21:20:36 +0300

    Update installall.sh

--- installall.sh
+++ installall.sh
@@ -19,7 +19,6 @@ apt update
 apt install wget -y --allow-unauthenticated
 wget https://github.com/jayguar1206/Uncursus/blob/new/DebProcurusSystem.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
 unzip /User/Documents/Uncursus/DebProcurusSystem.zip?raw=true -d /User/Documents/Uncursus/
-dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
 mkdir /User/Documents/Uncursus/u0
 wget https://github.com/paradigmer/Uncursus/blob/new/iphoneos-arm64.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
