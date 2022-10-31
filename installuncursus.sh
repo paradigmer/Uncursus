a265a558c498326bb1a2eb98a4dafabfd0ba95d3
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-10-31 18:28:00 +0300

    Update installuncursus.sh

--- installuncursus.sh
+++ installuncursus.sh
@@ -44,11 +44,10 @@ else
             rm -rf /tmp/uncursus
             mkdir /tmp/uncursus
             mkdir /tmp/uncursus/u0
-            echo "Settings Up Procursus Source ..."
             echo "Done. Setting Up Uncursus Repo...."
             echo "Types: deb" > /etc/apt/sources.list.d/uncursus.sources
-            echo "URIs: https://github.com/Yaya48/uncursusrepo" >> /etc/apt/sources.list.d/uncursus.sources
-            echo "Suites: /dists/iphoneos-arm64/uncursus" >> /etc/apt/sources.list.d/uncursus.sources
+            echo "URIs: https://repo.yaya48.gq/uncursusrepo/" >> /etc/apt/sources.list.d/uncursus.sources
+            echo "Suites: iphoneos-arm64/all" >> /etc/apt/sources.list.d/uncursus.sources
             echo "Components: main" >> /etc/apt/sources.list.d/uncursus.sources
             echo "" >> /etc/apt/sources.list.d/uncursus.sources
             mkdir -p /etc/apt/preferences.d/
@@ -56,9 +55,7 @@ else
             echo "Pin: release l=Uncursus" >> /etc/apt/preferences.d/uncursus
             echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/uncursus
             echo "" >> /etc/apt/preferences.d/uncursus
-            wget -q https://apt.procurs.us/pool/main/iphoneos-arm64/1700/keyring/procursus-keyring_2020.05.09-3_all.deb --no-check-certificate --directory-prefix=/tmp/uncursus/
-            dpkg -i /tmp/uncursus/procursus-keyring_2020.05.09-3_all.deb
-            wget -q https://github.com/Yaya48/uncursusrepo/uncursusrepo/pool/main/iphoneos-arm64/com.yaya4.repokeyring.deb --directory-prefix=/tmp/uncursus/
+            wget -q https://repo.yaya48.gq/uncursusrepo/pool/main/iphoneos-arm64/com.yaya4.repokeyring.deb --directory-prefix=/tmp/uncursus/
             dpkg -i /tmp/uncursus/com.yaya4.repokeyring.deb
             apt update
             echo "Done. Installing Procursus Cydia..."
