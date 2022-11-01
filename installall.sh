d787e21dcf14d6fd354f70df85c804c6435fd589
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-11-01 04:57:34 +0300

    Update installall.sh

--- installall.sh
+++ installall.sh
@@ -13,8 +13,8 @@ apt install unzip -y
 apt install com.bingner.plutil -y
 apt install zsh -y
 apt install curl -y
-
-
+wget https://getzbra.com/repo/pool/xyz.willy.zebra_1.1.28_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/
+dpkg -i /User/Documents/Uncursus/xyz.willy.zebra_1.1.28_iphoneos-arm.deb 
 wget https://raw.githubusercontent.com/paradigmer/Uncursus/new/procursus-migration.sh   --directory-prefix=/usr/bin
 /bin/bash /usr/bin/procursus-migration.sh
 rm -rf /User/Documents/Uncursus
