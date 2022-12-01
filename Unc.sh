84a1566978e5c3620a0458235d56f0ea17590269
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-12-02 01:19:13 +0300

    Update Unc.sh

--- Unc.sh
+++ Unc.sh
@@ -19,28 +19,16 @@ echo "Downloading And Installing Offical Procurus Deb"
 
 apt update
 apt install wget -y --allow-unauthenticated
-wget https://github.com/paradigmer/Uncursus/blob/new/DebProcurusSystem.zip --directory-prefix=/var/mobile/Documents/
-unzip /var/mobile/Documents/DebProcurusSystem.zip -d /var/mobile/Documents/
-dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
+dpkg -i /var/mobile/Documents/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
-mkdir /User/Documents/Uncursus/u0
-wget https://github.com/paradigmer/Uncursus/blob/new/iphoneos-arm64.zip --directory-prefix=/var/mobile/Documents/
-unzip /var/mobile/Documents/iphoneos-arm64.zip -d /var/mobile/Documents/
+
 rm -rf /usr/bin/cynject
 echo "Done. Installing necessary debs for patch."
 
-dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/coreutils-bin.deb
-dpkg -i --force-all /var/mobile/Documents/Diphoneos-arm64/libssl.deb
-dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/lzma.deb
-dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/xz.deb
-dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/libidn2.deb
-dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/ncurses5-libs.deb
+dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/*.deb
 echo "Done. Running Firmware Configuration (./firmware.sh)"
 /usr/libexec/firmware
 echo "BootStrap Installions Done. The Installer Clean The Installions"
-
-rm /etc/apt/sources.list.d/odyssey.sources
-echo "All Done."
 
 killall SpringBoard
 fi
