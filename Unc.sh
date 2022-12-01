7b34deec7aafcaa56a533e803e61b6cdbda4a9e9
paradigmer <59584564+paradigmer@users.noreply.github.com>
2022-12-01 05:58:10 +0300

    Update Unc.sh

--- Unc.sh
+++ Unc.sh
@@ -14,27 +14,27 @@ apt install com.bingner.plutil -y
 apt install zsh -y
 apt install curl -y
 echo "Downloading And Executing Offical Procurus Script From Coolstar"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/paradigmer/Uncursus/new/procursus-deploy-linux-macos.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Yaya48/Uncursus/new/procursus-migration.sh)"
 echo "Downloading And Installing Offical Procurus Deb"
 
 apt update
 apt install wget -y --allow-unauthenticated
-wget https://github.com/paradigmer/Uncursus/blob/new/DebProcurusSystem.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
-unzip /User/Documents/Uncursus/DebProcurusSystem.zip?raw=true -d /User/Documents/Uncursus/
+wget https://github.com/paradigmer/Uncursus/blob/new/DebProcurusSystem.zip --directory-prefix=/var/mobile/Documents/
+unzip /var/mobile/Documents/DebProcurusSystem.zip -d /var/mobile/Documents/
 dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
 mkdir /User/Documents/Uncursus/u0
-wget https://github.com/paradigmer/Uncursus/blob/new/iphoneos-arm64.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
-unzip /User/Documents/Uncursus/iphoneos-arm64.zip?raw\=true -d /User/Documents/Uncursus/DebPatch
+wget https://github.com/paradigmer/Uncursus/blob/new/iphoneos-arm64.zip --directory-prefix=/var/mobile/Documents/
+unzip /var/mobile/Documents/iphoneos-arm64.zip -d /var/mobile/Documents/
 rm -rf /usr/bin/cynject
 echo "Done. Installing necessary debs for patch."
 
-dpkg -i --force-all /var/mobile/Documents/DebPatch/coreutils-bin.deb
-dpkg -i --force-all /var/mobile/Documents/DebPatch/libssl.deb
-dpkg -i --force-all /var/mobile/Documents/DebPatch/lzma.deb
-dpkg -i --force-all /var/mobile/Documents/DebPatch/xz.deb
-dpkg -i --force-all /var/mobile/Documents/DebPatch/libidn2.deb
-dpkg -i --force-all /var/mobile/Documents/DebPatch/ncurses5-libs.deb
+dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/coreutils-bin.deb
+dpkg -i --force-all /var/mobile/Documents/Diphoneos-arm64/libssl.deb
+dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/lzma.deb
+dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/xz.deb
+dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/libidn2.deb
+dpkg -i --force-all /var/mobile/Documents/iphoneos-arm64/ncurses5-libs.deb
 echo "Done. Running Firmware Configuration (./firmware.sh)"
 /usr/libexec/firmware
 echo "BootStrap Installions Done. The Installer Clean The Installions"
